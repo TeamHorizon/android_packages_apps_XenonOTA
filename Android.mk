11d8dfb1c02a830f8d25a8994751c39a147b766d
Anton Hansson <hansson@google.com>
2018-08-20 17:24:15 +0200

    XenonOTA: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Iabb0556dc1c80c7fc7f6c76d61d5e441b03cdce0

--- Android.mk
+++ Android.mk
@@ -10,6 +10,8 @@ LOCAL_PACKAGE_NAME := XenonOTA
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
